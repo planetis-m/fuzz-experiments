d36c921b3acfa91cf822f2cc826c04574be7c2ec
Antonis Geralis <b3lievem@yandex.com>
2022-08-03 21:53:28 +0300

    total BS.

--- fuzzer_test_prototypes/rand_mutation.nim
+++ fuzzer_test_prototypes/rand_mutation.nim
@@ -1,6 +1,8 @@
 # Test the Post-script idea in
 # https://fitzgeraldnick.com/2019/09/04/combining-coverage-guided-and-generation-based-fuzzing.html
-import std/[random, math, fenv]
+import std/[random, math, fenv, sequtils]
+
+{.pragma: noCov, codegenDecl: "__attribute__((no_sanitize(\"coverage\"))) $# $#$#".}
 
 type
   GrowOrShrink = enum
@@ -36,7 +38,7 @@ proc mutate(x: var int64, r: var Rand,
     true
 
 proc mutate[T](x: var seq[T], r: var Rand,
-    growOrShrink: GrowOrShrink): bool =
+    growOrShrink: GrowOrShrink): bool {.noCov.} =
   case growOrShrink
   of GrowOrShrink.Grow:
     if x.len >= 10: return false
@@ -51,29 +53,61 @@ proc mutate[T](x: var seq[T], r: var Rand,
     for y in mitems(x):
       result = result and mutate(y, r, Shrink)
 
+#proc mutate(x: var float, r: var Rand,
+    #growOrShrink: GrowOrShrink): bool =
+  #let data = [
+    #minimumPositiveValue(float),
+    #maximumPositiveValue(float),
+    #-minimumPositiveValue(float),
+    #-maximumPositiveValue(float),
+    #-epsilon(float),
+    #epsilon(float),
+    #-Inf,
+    #Inf,
+    #0,
+    #r.rand(-1.0..1.0),
+  #]
+  #var tmp: array[data.len, float]
+  #case growOrShrink
+  #of GrowOrShrink.Grow:
+    #var i = 0
+    #for y in data.items:
+      #if y > x: tmp[i] = y; inc i
+    #x = if i > 0: tmp[r.rand(0..<i)] else: NaN
+    #true
+  #of GrowOrShrink.Shrink:
+    #var i = 0
+    #for y in data.items:
+      #if y < x: tmp[i] = y; inc i
+    #x = if i > 0: tmp[r.rand(0..<i)] else: NaN
+    #true
+
 proc mutate(x: var float, r: var Rand,
     growOrShrink: GrowOrShrink): bool =
-  let data = [
-    -Inf,
-    -maximumPositiveValue(float),
-    -minimumPositiveValue(float),
-    NaN,
-    r.rand(-1.0..1.0),
-    minimumPositiveValue(float),
-    maximumPositiveValue(float),
-    Inf
-  ]
-  case growOrShrink
-  of GrowOrShrink.Grow:
-    case classify(x)
-    of fcInf: return false
-    else: x = data[r.rand(1..high(data).int)]
-    true
-  of GrowOrShrink.Shrink:
-    case classify(x)
-    of fcNegInf: return false
-    else: x = data[r.rand(0..high(data)-1)]
-    true
+  result = true
+  case r.rand(10)
+  of 0:
+    x = NaN
+  of 1:
+    x = minimumPositiveValue(float)
+  of 2:
+    x = maximumPositiveValue(float)
+  of 3:
+    x = -minimumPositiveValue(float)
+  of 4:
+    x = -maximumPositiveValue(float)
+  of 5:
+    x = epsilon(float)
+  of 6:
+    x = -epsilon(float)
+  of 7:
+    x = Inf
+  of 8:
+    x = -Inf
+  of 9:
+    x = 0
+  else:
+    x = r.rand(-1.0..1.0)
 
 proc toUnstructured(data: ptr UncheckedArray[byte]; len: int): Unstructured =
   Unstructured(data: data, pos: 0, len: len)
@@ -87,27 +121,11 @@ proc readData(x: var Unstructured, buffer: pointer, bufLen: int) =
   zeroMem(buffer +! n, bufLen - n)
   inc(x.pos, n)
 
-proc writeData(x: var Unstructured, buffer: pointer, bufLen: int) =
-  if bufLen <= 0:
-    return
-  if x.pos + bufLen > x.len:
-    quitOrDebug()
-  copyMem(addr(x.data[x.pos]), buffer, bufLen)
-  inc(x.pos, bufLen)
-
 proc read[T](x: var Unstructured, result: var T) =
   readData(x, addr(result), sizeof(T))
 
 proc readInt32(x: var Unstructured): int32 =
   read(x, result)
-
-proc write[T](x: var Unstructured, v: T) =
-  writeData(x, addr v, sizeof(v))
-
-proc write(x: var Unstructured; v: seq[int32]) =
-  write(x, int32(v.len))
-  if v.len > 0:
-    writeData(x, addr v[0], v.len * sizeof(int32))
 
 proc intInRange[T: SomeInteger](u: var Unstructured; x: Slice[T]): T =
   assert(x.a <= x.b, "intInRange requires a non-empty range")
@@ -121,29 +139,29 @@ proc intInRange[T: SomeInteger](u: var Unstructured; x: Slice[T]): T =
     res = res mod (L + 1)
   result = cast[T](x.a.BiggestUInt + res)
 
-proc initFromBin[T](dst: var seq[T]; x: var Unstructured) =
+proc initFromBin[T](dst: var seq[T]; x: var Unstructured) {.noCov.} =
   let len = x.intInRange(0'i32..10'i32)
   dst.setLen(len)
   if len > 0:
     let bLen = len * sizeof(T)
     readData(x, dst[0].addr, bLen)
 
-proc byteSize[T](x: seq[T]): int =
-  result = sizeof(int32) + x.len * sizeof(T)
-
 proc sum(x: openArray[float]): float =
   result = 0.0
   for b in items(x):
     result = if isNaN(b): result else: result + b
 
+proc initRandom(seed: int64): Rand {.noCov.} = initRand(seed)
+
 proc testOneInput(data: ptr UncheckedArray[byte], len: int): cint {.
     exportc: "LLVMFuzzerTestOneInput", raises: [].} =
   result = 0
   if len < sizeof(int32): return
-  var x: seq[float]
+  var x: seq[float] = @[]
   var u = toUnstructured(data, len)
-  var r = initRand(len)
+  var r = initRandom(len)
   initFromBin(x, u)
   discard mutate(x, r, Shrink)
+  if x.len == 0: return
   let res = sum(x)
   if isNaN(res): echo "PANIC!"; quitOrDebug()
